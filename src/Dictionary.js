478b07a38167d1311797e41272bf35f3de0d5026
theaton89 <taylornheaton@gmail.com>
2021-06-17 17:00:11 -0500

    Added form functions to identify search words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function searchWord(event) {
+    event.preventDefault();
+    alert(`Searching for "${keyword}"`);
+  }
+
+  function handleWordSearch(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="Dictionary container">
-      <form className="row d-flex justify-content-center">
+      <form className="row d-flex justify-content-center" onSubmit={searchWord}>
         <input
           type="search"
           autoFocus={true}
           className="col-sm-3 search-button"
+          onChange={handleWordSearch}
         ></input>
 
         <input
